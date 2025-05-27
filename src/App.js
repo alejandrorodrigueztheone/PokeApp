bc8029c91e31fe7eee4750b53a01b37b98f276c6
alejandrorodrigueztheone <arodmed30@gmail.com>
2025-05-27 16:50:26 -0500

    Displaying name and sprite on html now

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { chooseRandomPokemon, getPokemonImage } from './index.js';
+import React, { useState } from 'react';
+
+function MyButton({ onChoose }) {
+  const handleClick = async () => {
+    const randomMon = await chooseRandomPokemon();
+    const image = await getPokemonImage(randomMon);
+    onChoose(randomMon, image); 
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Click for a Random Pokemon
+    </button>
+  );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonImage, setPokemonImage] = useState('');
+  return(
+   <div>
+      <MyButton onChoose={(pokemonName, pokemonImage) => {
+      setPokemonName(pokemonName);
+      setPokemonImage(pokemonImage);
+      }} />
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      {pokemonName && <h2>Your Pokémon: {pokemonName}</h2>}
+      {pokemonImage && <img src={pokemonImage} alt={pokemonName} style={{ marginTop: '20px', width: '150px' }} />}
+
     </div>
-  );
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -16,34 +16,41 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-/*//REMEMBER TO RUN BUILD TASK, press CNTRL + SHIFT + B
+//REMEMBER TO RUN BUILD TASK, press CNTRL + SHIFT + B
 //first we have to set up the connection to the API and be able to play around with it
 //second create a simple loop for finding a pokemon, or a specific type, or anything really in order to use it as a basis for the rest of the code that I am going to have to create
 
 //fetches all the names in the pokemon api and stores them in an array
-async function getAllPokemonNames(): Promise<string[]> {
+async function getAllPokemonNames() {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0');
     const data = await response.json();
-    const names = data.results.map((pokemon: { name: string }) => pokemon.name);
-    return names;
+    const names = data.results.map((pokemon=> pokemon.name))
+    return names
 }
 
+ 
 //basic randomizer
-function getRandomInt(max: number) {
+function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
 
 
 
 
-async function chooseRandomPokemon() {
+export async function chooseRandomPokemon() {
     const pokemonNames = await getAllPokemonNames();
     const randomPokemonIndex = getRandomInt(pokemonNames.length)
     const randomPokemon = pokemonNames[randomPokemonIndex]
     return randomPokemon
 }
 
-(async ( )=>{
-const randomPokemon = await chooseRandomPokemon()
-console.log(randomPokemon)
-})();*/
+export async function getPokemonImage(pokemon) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+  const data = await response.json();
+  return data.sprites.front_default; 
+//(async ( )=>{
+//const randomPokemon = await chooseRandomPokemon()
+//return randomPokemon
+//})()/
+
+}
